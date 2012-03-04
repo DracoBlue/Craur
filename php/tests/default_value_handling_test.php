2d328523fea358681271ac381b475eb9657c00dd
DracoBlue <JanS@DracoBlue.de>
2012-03-04 22:16:35 +0100

    added test for null/false as default value

--- php/tests/default_value_handling_test.php
+++ php/tests/default_value_handling_test.php
@@ -6,7 +6,17 @@ $xml_string = file_get_contents(dirname(__FILE__) . '/fixtures/working_feed.xml'
 $node = Craur::createFromXml($xml_string);
 
 assert((string) $node->get('feed.title') === 'Example Feed');
+
+/*
+ * Test if a string works as default value
+ */
 assert($node->get('feed.non_existant_key', 'default') === 'default');
+
+/*
+ * Test if null and false work, too!
+ */
+assert($node->get('feed.non_existant_key', null) === null);
+assert($node->get('feed.non_existant_key', false) === false);
 
 try
 {
