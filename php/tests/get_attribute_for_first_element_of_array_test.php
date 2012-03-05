adba2d631a8dc192550f7516ce28dd152d04aea0
DracoBlue <JanS@DracoBlue.de>
2012-03-05 09:44:39 +0100

    added failing testcases for #2 and #3

---
+++ php/tests/get_attribute_for_first_element_of_array_test.php
@@ -0,0 +1,24 @@
+<?php
+include(dirname(__FILE__) . '/../bootstrap_for_test.php');
+
+/*
+ * @see <https://github.com/DracoBlue/Craur/issues/3>
+ */
+
+$node = Craur::createFromJson(json_encode(array(
+    'book' => array(
+        'author' => array(
+            array(
+                '@name' => 'Hans'
+            ),
+            array(
+                '@name' => 'Paul'
+            ),
+        )
+    )
+)));
+
+/*
+ * Should be the first name
+ */
+assert('Hans' == $node->get('book.author.@name'));
---
+++ php/tests/parsing_atom_feed_test.php
@@ -0,0 +1,38 @@
+<?php
+include(dirname(__FILE__) . '/../bootstrap_for_test.php');
+
+$xml_string = file_get_contents(dirname(__FILE__) . '/fixtures/example_atom_feed.xml');
+$document = Craur::createFromXml($xml_string);
+
+/*
+ * Can we grab all feeds? (it should be just one)
+ */
+$feeds = $document->get('feed[]');
+assert(count($feeds) === 1);
+
+$feed = $document->get('feed');
+
+foreach ($feed->get('entry[]', array()) as $entry)
+{
+    /*
+     * At least 1 contributor
+     */
+    $contributors = $entry->get('contributor[]');
+    assert(count($contributors) > 0);
+    
+    /*
+     * first link.@rel must be alternate or self
+     */
+    assert(in_array($entry->get('link.@rel'), array('alternate', 'self')));
+    
+    $entry_data = $entry->getValues(
+        array(
+            'id' => 'id',
+            'title' => 'title',
+            'link' => 'link.@href',
+            'author_name' => 'author.name'
+        )
+    );
+    
+    assert(0 === count(array_diff(array_keys($entry_data), array('id', 'title', 'link', 'author_name'))));
+}
