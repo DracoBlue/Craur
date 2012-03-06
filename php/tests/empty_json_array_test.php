043806145ece8f86c2fe58c9ca14f78dbd2de5e2
DracoBlue <JanS@DracoBlue.de>
2012-03-06 22:41:33 +0100

    added test for the default case

--- php/tests/empty_json_array_test.php
+++ php/tests/empty_json_array_test.php
@@ -31,3 +31,8 @@ catch (Exception $exception)
      * Yes, we expected it to fail since there is no value at items!
      */
 }
+
+/*
+ * But it should work with default value!
+ */
+$node->get('items', "me item!");
