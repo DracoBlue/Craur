ea03e3fd721d19e695d2dacfcf3e101515734be2
DracoBlue <JanS@DracoBlue.de>
2012-03-06 22:16:08 +0100

    disabled clover, you can comment it in, if you need it.

--- run_tests.sh
+++ run_tests.sh
@@ -23,7 +23,8 @@ do
 done
 
 cd ../../
-php php/create_clover_xml_for_raw_coverage_data.php $raw_code_coverage_file $clover_file
+#enable clover for your project, if you need it. so long, disabled!
+#php php/create_clover_xml_for_raw_coverage_data.php $raw_code_coverage_file $clover_file
 echo ""
 php php/output_summary_for_raw_coverage_data.php $raw_code_coverage_file $clover_file
 rm $raw_code_coverage_file
