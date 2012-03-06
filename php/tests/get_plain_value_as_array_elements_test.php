086b94ec1046c2e616d0e28b17bd95d98fa55eab
DracoBlue <JanS@DracoBlue.de>
2012-03-06 22:51:26 +0100

    added test to retrieve the plain value as array of items

---
+++ php/tests/get_plain_value_as_array_elements_test.php
@@ -0,0 +1,12 @@
+<?php
+include(dirname(__FILE__) . '/../bootstrap_for_test.php');
+
+$node = Craur::createFromJson('{"items":"hans"}');
+
+/*
+ * Even though items is no numeric array and contains directly
+ * the member, we want to get it as items!
+ */
+$items = $node->get('items[]');
+assert(count($items) == 1);
+assert($items[0] == "hans");
