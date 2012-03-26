9a86accd3610e996cb674bc5cb21e07cb996c031
DracoBlue <JanS@DracoBlue.de>
2012-03-26 10:20:19 +0200

    added failing test for nonexistant sub sub entry keys

---
+++ php/tests/csv_output_with_missing_sub_sub_entry_test.php
@@ -0,0 +1,24 @@
+<?php
+
+$craur = Craur::createFromXml(file_get_contents('fixtures/example_atom_feed.xml'));
+
+$craur->saveToCsvFile('fixtures/temp_csv_file.csv', array(
+    'feed.entry[].title',
+    'feed.entry[].link[].@rel',
+    'feed.entry[].link[].nonexistantsubkey[].name',
+    'feed.entry[].link[].@href',
+));
+
+$result_csv_content = trim(file_get_contents('fixtures/temp_csv_file.csv'));
+unlink('fixtures/temp_csv_file.csv');
+
+$lines = array_slice(explode(PHP_EOL, $result_csv_content), 1);
+
+$expected_lines = array(
+    '"Atom draft-07 snapshot";alternate;;http://example.org/2005/04/02/atom',
+    '"Atom draft-07 snapshot";enclosure;;http://example.org/audio/ph34r_my_podcast.mp3'
+);
+
+assert(count(array_diff($lines, $expected_lines)) === 0);
+
+
